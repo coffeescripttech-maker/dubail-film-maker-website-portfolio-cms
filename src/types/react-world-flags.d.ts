719b0ae6a034e01682fa27cc82c2904bafa5ea9c
xedter20 <dextermiranda441@gmail.com>
2025-12-29 16:05:04 +0800

    fixes

---
+++ src/types/react-world-flags.d.ts
@@ -0,0 +1,15 @@
+declare module 'react-world-flags' {
+  import { ComponentType } from 'react';
+
+  interface FlagProps {
+    code?: string;
+    height?: string | number;
+    width?: string | number;
+    className?: string;
+    style?: React.CSSProperties;
+    fallback?: React.ReactNode;
+  }
+
+  const Flag: ComponentType<FlagProps>;
+  export default Flag;
+}
